24c263bb05f853401b2961d4bf8f9f4d2ae37090
chriseldhovarghese <chriseldhovarghese@gmail.com>
2021-01-02 18:08:51 -0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Student chris = new Student ("Chris", 7, 59);
+        Student chris = new Student("Chris", 7, 59);
         Student sony = new Student("Sony", 3, 63);
-        Student natalie = new Student("Natalie", 6,56);
+        Student natalie = new Student("Natalie", 6, 56);
         Student allen = new Student("Allen", 21, 65);
 
         Student[] students = {chris, sony, natalie, allen};
@@ -10,8 +10,7 @@ public class Main {
 
         StudentSort.sortStudentBasedOnClass(students);
 
-        for (Student student:students)
-        {
+        for (Student student : students) {
             System.out.println(student.getName());
         }
     }
--- src/StudentSort.java
+++ src/StudentSort.java
@@ -1,10 +1,7 @@
 public class StudentSort {
-    public static void sortStudentBasedOnClass(Student[] students)
-    {
-        for (int i=0; i < students.length - 1; i++)
-        {
-            for (int j = i+1; j < students.length; j++)
-            {
+    public static void sortStudentBasedOnClass(Student[] students) {
+        for (int i = 0; i < students.length - 1; i++) {
+            for (int j = i + 1; j < students.length; j++) {
                 if (students[i].getClassNumber() > students[j].getClassNumber()) {
                     final Student temporaryStore = students[i];
                     students[i] = students[j];
